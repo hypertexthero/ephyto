4199061df5465cfc059557b079f07fc070b4e13a
hypertexthero <simon@hypertexthero.com>
2012-12-20 15:34:01 +0100

    automatically set site in admin

--- core/admin.py
+++ core/admin.py
@@ -3,7 +3,14 @@ from django.contrib import admin
 from django.contrib.flatpages.models import FlatPage
 from django.contrib.flatpages.admin import FlatPageAdmin, FlatpageForm
 
+# Make sites selected by default - http://stackoverflow.com/a/5669589/412329
+from django.contrib.sites.models import Site
 class FlatPageAdmin(admin.ModelAdmin):
+	def formfield_for_manytomany(self, db_field, request=None, **kwargs):
+            if db_field.name == "sites":
+                kwargs["initial"] = [Site.objects.get_current()]
+            return super(FlatPageAdmin, self).formfield_for_foreignkey(db_field, request, **kwargs)
+	
 	exclude = ('enable_comments','template_name',)
 	save_on_top = True
 	pass
--- core/models.py
+++ core/models.py
@@ -1,2 +1,8 @@
-from django.db import models
-
+# from django.db import models
+# from django.contrib.sites.models import Site
+# # http://stackoverflow.com/questions/1021487/add-functionality-to-django-flatpages-without-changing-the-original-django-app
+# from django.contrib.flatpages.models import FlatPage as FlatPageOld
+# 
+# class FlatPage(FlatPageOld):
+#     # order = models.PositiveIntegerField(unique=True)
+#     sites = models.ManyToManyField(Site, default=Site.objects.get_current())
